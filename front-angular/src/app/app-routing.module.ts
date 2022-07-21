e98cc588157e0a829580b3db47b8f7f53cb99f80
JoseLuiz <zzeris@gmail.com>
2022-07-21 09:17:01 -0300

    Add cards module, component and routing

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'cards'
+  },
+  {
+    path: 'cards',
+    loadChildren: () => import('./cards/cards.module').then(m => m.CardsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
